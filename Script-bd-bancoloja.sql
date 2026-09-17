d3a6c8724dc04983e706bf6807fab7a123772ba2
max-DS-kkj <l.max@escola.pr.gov.br>
2026-09-17 10:32:58 -0300

    tabela pagamento atualizacao

--- Script-bd-bancoloja.sql
+++ Script-bd-bancoloja.sql
@@ -1,3 +1,8 @@
 CREATE TABLE pagamento(
   id_pagamento SERIAL PRIMARY KEY,
-  tipo varchar(50),
+  tipo varchar(50) NOT NULL,
+  valor decimal(10,2) NOT NULL,
+  status varchar(59),
+  id_venda bigint NOT NULL
+);
+
