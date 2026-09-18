1037a5f2f68645cc4a749d4aa1f2e5412c147e8c
max-DS-kkj <l.max@escola.pr.gov.br>
2026-09-18 07:43:11 -0300

    atualicacao do codigo certo

--- Script-bd-bancoloja.sql
+++ Script-bd-bancoloja.sql
@@ -1,22 +1,15 @@
 CREATE TABLE pagamento(
   id_pagamento SERIAL PRIMARY KEY,
-  tipo varchar(50) NOT NULL,
-  valor decimal(10,2) NOT NULL,
-  status varchar(59),
-  id_venda bigint NOT NULL
+  forma_pagamento varchar(50) NOT NULL,
+  taxa_entrega decimal(10,2),
+  valor_pago decimal(10,2) NOT NULL,
+  status_pagamento varchar(50),
+  id_compra bigint NOT NULL
 );
 
 CREATE TABLE categoria(
   id_categoria SERIAL PRIMARY KEY,
   nome varchar(100) NOT NULL,
-  descricao varchar(255)
+  descricao text
 );
 
-CREATE TABLE venda(
-  id_venda SERIAL PRIMARY KEY,
-  data datetime,
-  valor_total decimal(10,2) NOT NULL,
-  origim varchar(255),
-  id_cliente bigint NOT NULL,
-  id_loja bigint NOT NULL
-);
